a0a9b87211c7633cfcd28ce1cd905fdc1db02feb
Katie Reed <katharynreedpierce@gmail.com>
2017-02-17 09:33:48 -0800

    refactor count_repeats function for more specificity

--- src/RepeatCounter.php
+++ src/RepeatCounter.php
@@ -3,7 +3,7 @@
     class RepeatCounter
     {
 
-        public $count_return;
+        public $count_return = 0;
 
         function string_check($input) {
             if ($input) {
@@ -14,11 +14,21 @@
             }
         }
 
-        function count_repeats($input1, $input2) {
-            $count_return = substr_count(strtolower($input1), strtolower($input2));
+        function count_repeats($string, $word) {
+            $string_to_array = explode(" ", $string);
+            foreach ($string_to_array as $input) {
+                if ($word == $input) {
+                    $count_return ++;
+                }
+            }
             return $count_return;
-
         }
+
+        // function count_repeats($input1, $input2) {
+        //     $count_return = substr_count(strtolower($input1), strtolower($input2));
+        //     return $count_return;
+        //
+        // }
     }
 
 

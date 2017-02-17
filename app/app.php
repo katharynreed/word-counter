7aed9deace6bcc09761abb150493e02aa20e7c94
Katie Reed <katharynreedpierce@gmail.com>
2017-02-17 10:06:49 -0800

    add results.html and complete iniital app build

--- app/app.php
+++ app/app.php
@@ -1,13 +1,30 @@
 <?php
-    require_once __DIR__."/../vendor/autoload.php";
-    require_once __DIR__."/../src/RepeatCounter.php";
+    date_default_timezone_set('America/Los_Angeles');
+    require_once __DIR__.'/../vendor/autoload.php';
+    require_once __DIR__.'/../src/RepeatCounter.php';
     session_start();
     if (empty($_SESSION['list_of_matches'])) {
         $_SESSION['list_of_matches'] = array();
     }
-    $app["debug"] = true;
+    $app['debug'] = true;
     $app = new Silex\Application();
     $app->register(new Silex\Provider\TwigServiceProvider(), ['twig.path' => __DIR__.'/../views']);
-    $app->get("/", function() use($app) {
-        return $app["twig"]->render("homepage.html.twig");
+
+
+    $app->get('/', function() use ($app) {
+        return $app['twig']->render('homepage.html.twig');
     });
+
+    $app->post('/count', function() use ($app){
+        $newRepeatCounter = new RepeatCounter;
+        $string = ($_POST['string']);
+        $word = ($_POST['word']);
+
+        $result = $newRepeatCounter->string_check($string);
+        $result = $newRepeatCounter->count_repeats($string, $word);
+
+        return $app['twig']->render('results.html.twig', array('result' => $result));
+    });
+
+    return $app;
+?>

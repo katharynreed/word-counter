21ac79d0c627242c9c92e84d1565ef78d7f5f9c6
Katie Reed <katharynreedpierce@gmail.com>
2017-02-17 11:00:51 -0800

    change response message

--- src/RepeatCounter.php
+++ src/RepeatCounter.php
@@ -15,13 +15,24 @@
         }
 
         function count_repeats($string, $word) {
+            $string = strtolower($string);
+            $word = strtolower($word);
             $string_to_array = explode(" ", $string);
             foreach ($string_to_array as $input) {
                 if ($word == $input) {
                     $this->count_return ++;
                 }
             }
-            return $this->count_return;
+            $word = strtoupper($word);
+            if ($this->count_return == 1) {
+                return $this->count_return.' instance of '.$word;
+            }
+            elseif (($this->count_return > 1) && ($this->count_return < 10)) {
+                return $this->count_return.' instances of '.$word;
+            }
+            else {
+                return $this->count_return.' instances of '.$word.', you should buy a thesuarus for sure.';
+            }
         }
 
         function get($property)
